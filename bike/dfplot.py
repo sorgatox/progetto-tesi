f20e1375a2f684b91ed57e681b0b4eed9307c188
sorgatox <enricosorgato0310@gmail.com>
2025-07-08 11:38:06 +0200

    aggiunto grafico a 3 barre - weekday

--- bike/dfplot.py
+++ bike/dfplot.py
@@ -50,6 +50,35 @@ def plot_total_rides_comparison(df, df_dp, eps, num):
     plt.tight_layout()
     plt.savefig(f"bike/weekplt/plt{eps}_{num}.png")
 
+def plot_total_rides_comparison2(df, df_dp1, df_dp2, eps1, eps2, num):
+    weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    df = df.sort("weekday", descending=False)
+    df_dp1 = df_dp1.sort("weekday", descending=False)
+    df_dp2 = df_dp2.sort("weekday", descending=False)
+
+    total_raw = df["len"].to_list()
+    total_dp1 = df_dp1["len"].to_list()
+    total_dp2 = df_dp2["len"].to_list()
+
+    x = range(len(weekdays))
+    width = 0.25
+
+    fig, ax = plt.subplots(figsize=(12, 6))
+    ax.bar([i - width for i in x], total_raw, width=width, label="Senza DP")
+    ax.bar([i for i in x], total_dp1, width=width, label=f"DP (ε = {eps1})")
+    ax.bar([i + width for i in x], total_dp2, width=width, label=f"DP (ε = {eps2})", color = "#00b300")
+
+    ax.set_ylabel("Corse totali")
+    ax.set_title(f"Confronto delle corse totali per giorno della settimana\n(ε = {eps1}, ε = {eps2}, size = {num})")
+    ax.set_xticks(x)
+    ax.set_xticklabels(weekdays, rotation=45, ha="right")
+    ax.legend()
+    ax.grid(axis="y", linestyle="--", alpha=0.7)
+
+    plt.tight_layout()
+    plt.savefig(f"bike/weekplt/plt_{eps1}_{eps2}_{num}.png")
+
+
 
 def plot_acceps(df = pl.read_csv("bike/samplecsv/eps_1000.csv")):
     epsilons = df["epsilon"].round(3).to_list()
