50ef0362a3427b8249ec48e4deaf74aab663c952
sorgatox <enricosorgato0310@gmail.com>
2025-07-09 10:59:23 +0200

    esperimento - sample 200mila

--- bike/weekday.py
+++ bike/weekday.py
@@ -27,7 +27,7 @@ bike = pl.read_csv(
 bike = pre.cast_enum(bike)
 
 #sample dei dati
-num = 500000
+num = 200000
 bike = bike.sample(n = num, with_replacement = False, seed = 1234)
 
 #DP dataset
@@ -50,11 +50,11 @@ mean_diff = week_diff.select(pl.col("diff").mean())
 print(f"Totale corse per giorno della settimana: {trips_weekday}")
 print(f"Differenza (MIN, MEAN e MAX) tra i conteggi per giorno della settimana: {[min_diff.item(), mean_diff.item(), max_diff.item()]}")
 
-filename = f"bike/samplecsv/diff_{num}.csv"
-week_diff.write_csv(filename)
+'''filename = f"bike/samplecsv/diff_{num}.csv"
+week_diff.write_csv(filename)'''
 
 #invariant keys - giorni della settimana sono pubblici
-eps = 0.01
+eps = 0.1
 context = dp.Context.compositor(
     data = df,
     privacy_unit = dp.unit_of(contributions=1),
@@ -77,7 +77,7 @@ print(f"Totale corse per giorno della settimana con DP, eps = {eps}: {result}")
 print(query_counts.summarize(alpha=0.05))
 
 #invariant keys - giorni della settimana sono pubblici
-eps1 = 0.001
+eps1 = 0.01
 context1 = dp.Context.compositor(
     data = df,
     privacy_unit = dp.unit_of(contributions=1),
@@ -99,8 +99,34 @@ result1 = result1.with_columns(pl.col("len").cast(pl.Int64))
 print(f"Totale corse per giorno della settimana con DP, eps = {eps1}: {result1}")
 print(query_counts1.summarize(alpha=0.05))
 
+#invariant keys - giorni della settimana sono pubblici
+eps2 = 0.001
+context2 = dp.Context.compositor(
+    data = df,
+    privacy_unit = dp.unit_of(contributions=1),
+    privacy_loss = dp.loss_of(epsilon=eps2),
+    split_evenly_over = 1,
+    margins = [dp.polars.Margin(by=["weekday"], public_info="keys")]            
+)
+
+query_counts2 = (
+    context2.query()
+    .group_by("weekday")
+    .agg(dp.len())
+)
+result2 = query_counts2.release().collect().sort("weekday")
+result2 = result2.with_columns(pl.col("len").cast(pl.Int64))
+
+
+
+print(f"Totale corse per giorno della settimana con DP, eps = {eps2}: {result2}")
+print(query_counts2.summarize(alpha=0.05))
+
 
 #crea grafici
 dfplot.plot_total_rides_comparison(trips_weekday, result, eps, num)
 dfplot.plot_total_rides_comparison(trips_weekday, result1, eps1, num)
+dfplot.plot_total_rides_comparison(trips_weekday, result2, eps2, num)
 dfplot.plot_total_rides_comparison2(trips_weekday, result, result1, eps, eps1, num)
+dfplot.plot_total_rides_comparison2(trips_weekday, result, result2, eps, eps2, num)
+dfplot.plot_total_rides_comparison3(trips_weekday, result, result1, result2, eps, eps1, eps2, num)
